1904cc3fcdeeb66ba14a25abca74855a35d818c8
Michael Yuan <michael@secondstate.io>
2023-07-27 05:49:34 +0000

    fix compiler errors
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+use serde_json::json;
 use serde_json::Value;
 use tg_flows::{listen_to_update, Telegram, Update, UpdateKind, InputFile};
 use store_flows::{get, set};
 use flowsnet_platform_sdk::logger;
+use url::Url;
 use http_req::{
     request::{Method, Request},
     uri::Uri,
 };
+use tokio::time::{sleep, Duration};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -14,7 +17,7 @@ pub async fn run() -> anyhow::Result<()> {
     let telegram_token = std::env::var("telegram_token").unwrap();
     let api_key = std::env::var("miaoshouai_key").unwrap();
     let placeholder_text = std::env::var("placeholder").unwrap_or("Generating your image ...".to_string());
-    let help_mesg = std::env::var("help_mesg").unwrap_or("Type command `/model model_name` to select a model. Available choices for `model_name` are: redshift-diffusion, samdoesarts-ultmerge,  midjourney-v4, inkpunk".to_string());
+    let help_mesg = std::env::var("help_mesg").unwrap_or("Type command `/model_id` to select a model. Available choices are: /redshift-diffusion, /samdoesarts-ultmerge,  /midjourney-v4, /inkpunk".to_string());
 
     listen_to_update(&telegram_token, |update| {
         let tele = Telegram::new(telegram_token.to_string());
@@ -30,15 +33,61 @@ async fn handler(tele: Telegram, api_key: &str, placeholder_text: &str, help_mes
         log::info!("Received message from {}", chat_id);
 
         let text = msg.text().unwrap_or("");
-        if text.eq_ignore_ascii_case("/help") {
+        if text.eq_ignore_ascii_case("/help") || text.eq_ignore_ascii_case("/start") {
             _ = tele.send_message(chat_id, help_mesg);
 
-        if text.starts_with("/model ") {
-            let model_id = text.strip_prefix("/model ").unwrap_or("");
-            if model_id.is_empty() {
-                _ = tele.send_message(chat_id, "Sorry the model ID is not recognized.");
-                return;
-            }
+        } else if text.eq_ignore_ascii_case("/redshift-diffusion") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/samdoesarts-ultmerge") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/midjourney-v4") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/inkpunk") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/synthwave-diffusion") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/analog-diffusion") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/elldreths-vi") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/dreamlike") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/dream-shaper-8797") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/counterfeit-v20") {
+            let model_id = text.strip_prefix("/").unwrap();
+            set("model_id", json!(model_id), None);
+            _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
+
+        } else if text.eq_ignore_ascii_case("/deliberate") {
+            let model_id = text.strip_prefix("/").unwrap();
             set("model_id", json!(model_id), None);
             _ = tele.send_message(chat_id, &format!("The model has been set to {}", model_id));
 
@@ -66,43 +115,26 @@ async fn handler(tele: Telegram, api_key: &str, placeholder_text: &str, help_mes
                 }
             }
 
-            let json_template = r#"
-{
-  "apiKey": "{}",
-  "batch_size": "1",
-  "height": 512,
-  "width": 512,
-  "model_id": "{}",
-  "prompt": "{}",
-  "negative_prompt": "{}",
-  "num_inference_steps": 20,
-  "samplers": "EulerAncestralDiscreteScheduler",
-  "seed": 0,
-  "enhance_prompt": "no",
-  "cfg_scale": 7.5
-}"#;
-            let json_request = format!(json_template, api_key, model_id, positive_prompt, positive_prompt);
+            let json_request = format!(include_str!("request_template.json"), api_key, model_id, positive_prompt, negative_prompt);
             let api_uri: Uri = Uri::try_from("https://miaoshouai.com/playground/translation/produce/do/text2img").unwrap();
             let mut bytes = Vec::new();
             Request::new(&api_uri)
                 .method(Method::POST)
-                .body(&json_request)
+                .body(json_request.as_bytes())
                 .send(&mut bytes).unwrap();
             let json_response = String::from_utf8(bytes).unwrap();
             log::info!("Received from web service : {}", json_response);
 
             // wait for 15 sec!
+            sleep(Duration::from_millis(20000)).await;
             
             let c: Value = serde_json::from_str(&json_response).unwrap();
             let fetch_key = c["data"]["fetchKey"].as_str().unwrap();
-            let fetch_url = format!("https://miaoshouai.com/playground/translation/produce/get/fetchResult?fetchKey={}", fetch_key);
-            _ = tele.send_photo(chat_id, InputFile::url(&fetch_url));
-
-        } else {
-            // Do nothing
+            let fetch_url = Url::parse(&format!("https://miaoshouai.com/playground/translation/produce/get/fetchResult?fetchKey={}", fetch_key)).unwrap();
+            _ = tele.send_photo(chat_id, InputFile::url(fetch_url));
+            _ = tele.edit_message_text(chat_id, placeholder.id, "");
         }
 
         return;
-
     }
 }
